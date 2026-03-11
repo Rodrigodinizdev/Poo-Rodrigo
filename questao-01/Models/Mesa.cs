ad7ea60086bc0b84cdf01abab9a31f86b3e357bb
Rodrigo Diniz <rodrigodiniz.developer@gmail.com>
2026-03-10 21:23:25 -0300

    questao thomaz

---
+++ questao-01/Models/Mesa.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaliacaoInstrutoresQuestao_01.Models;
+
+public class Mesa(int id, int capacidadeDaMesa)
+{
+    public int Id { get; } = id;
+    public int CapacidadeDaMesa { get; } = capacidadeDaMesa;
+}
---
+++ questao-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using AvaliacaoInstrutoresQuestao_01.Models;
+
+var notificationHandler = new NotificationHandler();
+var restaurante = new Restaurante(notificationHandler);
+var clienteFactory = new ClienteFactory(notificationHandler);
+
+restaurante.AdicionarMesa(capacidadeDaMesa: 8);
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+restaurante.AdicionarMesa(capacidadeDaMesa: 6);
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+var cliente1 = clienteFactory.CriarCliente(id: 1, nome: "Carlos", telefoneValue: "123456789");
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+var cliente2 = clienteFactory.CriarCliente(id: 2, nome: "Maria", telefoneValue: "987654321");
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+restaurante.ReservarMesa(cliente: cliente2, numeroDePessoas: 10);
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+restaurante.ReservarMesa(cliente: cliente1, numeroDePessoas: 6);
+if (notificationHandler.HasErrors())
+    notificationHandler.GetAndClearErrors();
+
+
